fb34ffc7809fcb3fa9ac0163cf18ebf914d6ca56
Ellen Waddle <elwa8931@colorado.edu>
2020-04-09 11:33:13 -0600

    added random landscape, agents starting on perimeter, tracking time spent

--- run_and_tumble_mod2.m
+++ run_and_tumble_mod2.m
@@ -1,6 +1,34 @@
 %first attempt at combining agents and landscape for a simple run and
 %tumble model. 
 
+
+%Set variable model parameters
+%initialize variables for move_and_feed_1
+feed_time = 1; %relative to total movement time (of 1)
+boundary = 8;
+feed_amount = 5;
+init_fullness = 10;
+
+vary_angle_or_dist = 0; %set tumble strategy: 0: dist, 1: dist
+max_turn_angle = pi;
+min_tumb = 0.5;
+max_tumb = 2;
+run_tumb_ratio = 4;
+min_run = min_tumb * run_tumb_ratio;
+max_run = max_tumb * run_tumb_ratio;
+
+%parameters for decision-making
+stay_grass = 30;
+stay_nutrition = 4;
+run_nutrition = 6;
+
+xdim = 50; %dimensions of landscape
+ydim = 50;
+steps = 1000; %set max time steps
+num_animals = 10; %set number of animals to walk the Earth
+
+
+
 %%STEP 1: initialize landscape using initialize_landscape_1.m
 xdim = 50;
 xdim = ydim;  
@@ -11,6 +39,7 @@ feed_amount = 5;
 steps = 2000; %set max time steps
 num_animals = 10 ;%set number of animals to walk the Earth
 
+<<<<<<< HEAD
 
 [X,Y] = meshgrid(linspace(1,xdim,n_mounds/2),linspace(1,ydim,n_mounds/2));
 uniform_fert = round([X(:), Y(:)]);
@@ -28,6 +57,16 @@ landscape = initialize_landscape_1(xdim, ydim, fertilizer_xy);
 %STEP2: agents move through landscape.
 
 
+=======
+[X,Y] = meshgrid(linspace(1,45,5),linspace(1,45,5));
+fertilizer_xy = round([X(:), Y(:)]);
+scatter(X(:),Y(:))
+landscape = initialize_landscape_1(xdim, ydim, fertilizer_xy);
+
+
+%STEP2: agents move through landscape.
+
+>>>>>>> c62e714c22494e2977b924caf128a925891c559c
 
 %Record trajectory of all animals. First two columns are first animal,
 %third and fourth columns are second animal, etc.
@@ -38,7 +77,7 @@ time_until_leaving = zeros(num_animals);
 
 for animal = 1:num_animals
     %%movement loop
-    animal_x = 2*animal - 1;
+    animal_x = 2*animal - 1;%These are for indexing trajectories array
     animal_y = animal_x + 1;
     
     %random starting position on perimeter of landscape: 
@@ -61,29 +100,39 @@ for animal = 1:num_animals
         y1 = curr_location(2);
         [grass_quantity, nutrition] = current_location(landscape,x1, y1);
 
+<<<<<<< HEAD
         if  grass_quantity > 30 && nutrition > 40 %add something about nutrition in here
+=======
+        if  grass_quantity > stay_grass && nutrition > stay_nutrition
+>>>>>>> c62e714c22494e2977b924caf128a925891c559c
             %disp('stay')
             x2 = curr_location(1);
             y2 = curr_location(2);
-             %update landscape
-
-
+             
         else
            % disp('move')
+<<<<<<< HEAD
             turning_angle =  unifrnd(0,2*pi); %uniform dist 0->360 deg
 
             if nutrition < 40 %move farther if nutrition is low
                 d = unifrnd(2, 4); %uniform dist of step size
+=======
+            turning_angle =  unifrnd(-max_turn_angle, max_turn_angle); %uniform dist within bounds
+            
+            if nutrition < run_nutrition %move farther if nutrition is low
+                d = unifrnd(min_run, max_run); %uniform dist of step size
+>>>>>>> c62e714c22494e2977b924caf128a925891c559c
             else  %stay closer if nutrition is high
-                d = unifrnd(0.5, 2);
+                d = unifrnd(min_tumb, max_tumb);
             end 
-
+            
             curr_location(1) = curr_location(1)+d*sin(turning_angle);
             curr_location(2) = curr_location(2)+d*cos(turning_angle); %agent moves 
             x2 = (curr_location(1));
             y2 = (curr_location(2));
-
+            
         end 
+<<<<<<< HEAD
 
 
         [landscape, grass_consumed, nutrition, leave] = move_and_feed_1(landscape,...
@@ -92,6 +141,16 @@ for animal = 1:num_animals
              if leave == 1
             trajectories( (t+1 : steps+1), (animal_x : animal_y) ) = NaN;
             time_until_leaving(animal) = t;
+=======
+        
+        %Update landscape and trajectories array
+        [landscape, grass_consumed, nutrition, leave] = move_and_feed_1(landscape,...
+                x1, y1, x2, y2, boundary, feed_amount, feed_time);
+        if leave == 1
+            for remaining_steps = t+1 : steps+1
+                trajectories(remaining_steps, animal_x : animal_y) = NaN;
+            end
+>>>>>>> c62e714c22494e2977b924caf128a925891c559c
             break
             %ends "t" loop. returns to "animal" loop.
        end
