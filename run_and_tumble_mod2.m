3a60b25208cb35e05e8c0bf1ed0691cea58dbe71
Ellen Waddle <elwa8931@colorado.edu>
2020-04-09 11:29:56 -0600

    added t spent by each agent, released agents from edge, option for random termite mound locations

--- run_and_tumble_mod2.m
+++ run_and_tumble_mod2.m
@@ -2,36 +2,37 @@
 %tumble model. 
 
 %%STEP 1: initialize landscape using initialize_landscape_1.m
-%function landscape = initialize_landscape_1(x_dim, y_dim, fertilizer_xy)
-%fertilizer_xy = [1,1;2,2;6,5;39,31;49,43;29,39;10,10]; %made up for now.
-
-[X,Y] = meshgrid(linspace(1,45,5),linspace(1,45,5));
-fertilizer_xy = round([X(:), Y(:)]);
-scatter(X(:),Y(:))
-
-%plot(xpts,ypts);
-%scatter(spots);
 xdim = 50;
-ydim = 50;
+xdim = ydim;  
+n_mounds = 10;
+feed_time = 1; 
+boundary = 8;
+feed_amount = 5; 
+steps = 2000; %set max time steps
+num_animals = 10 ;%set number of animals to walk the Earth
+
+
+[X,Y] = meshgrid(linspace(1,xdim,n_mounds/2),linspace(1,ydim,n_mounds/2));
+uniform_fert = round([X(:), Y(:)]);
+
+random_fert = [randi([1 xdim],1,n_mounds) ; randi([1 ydim],1,n_mounds)];
+random_fert = transpose(random_fert);
+
+
+fertilizer_xy = random_fert; %options: uniform_fert or random_fert 
 
 landscape = initialize_landscape_1(xdim, ydim, fertilizer_xy);
 
-%initialize variables for move_and_feed_1
-feed_time = 1; %always
-boundary = 8; %always
-feed_amount = 5; %always
 
 
 %STEP2: agents move through landscape.
 
-%set max time steps
-steps = 1000;
-%set number of animals to walk the Earth
-num_animals = 10;
+
 
 %Record trajectory of all animals. First two columns are first animal,
 %third and fourth columns are second animal, etc.
 trajectories = zeros(steps+1, 2*num_animals);
+time_until_leaving = zeros(num_animals);
 
 
 
@@ -39,7 +40,16 @@ for animal = 1:num_animals
     %%movement loop
     animal_x = 2*animal - 1;
     animal_y = animal_x + 1;
-    trajectories(1, animal_x : animal_y) = [0.5*xdim,0.5*ydim]; %start at center XY
+    
+    %random starting position on perimeter of landscape: 
+    %pick x or y to start on, other var is 1 or max. 
+    A = [1, xdim];
+    astart = A(randi(length(A), 1));
+    bstart = (randi(xdim, 1));
+    starting_pos = [astart,bstart ; bstart,astart ]; 
+    start_pos = starting_pos(:,randi(2,1));
+    
+    trajectories(1, animal_x : animal_y) = [start_pos]; 
     curr_location = zeros(1,2);
     %initialize. Goes to 1 when animal leaves boundary on landscape.
     leave = 0;
@@ -51,7 +61,7 @@ for animal = 1:num_animals
         y1 = curr_location(2);
         [grass_quantity, nutrition] = current_location(landscape,x1, y1);
 
-        if  grass_quantity > 30 && nutrition > 4 %add something about nutrition in here
+        if  grass_quantity > 30 && nutrition > 40 %add something about nutrition in here
             %disp('stay')
             x2 = curr_location(1);
             y2 = curr_location(2);
@@ -62,8 +72,8 @@ for animal = 1:num_animals
            % disp('move')
             turning_angle =  unifrnd(0,2*pi); %uniform dist 0->360 deg
 
-            if nutrition < 6 %move farther if nutrition is low
-                d = unifrnd(2, 8); %uniform dist of step size
+            if nutrition < 40 %move farther if nutrition is low
+                d = unifrnd(2, 4); %uniform dist of step size
             else  %stay closer if nutrition is high
                 d = unifrnd(0.5, 2);
             end 
@@ -75,18 +85,17 @@ for animal = 1:num_animals
 
         end 
 
-            %NEED TO UPDATE THE location array after taurus as well!!!
 
         [landscape, grass_consumed, nutrition, leave] = move_and_feed_1(landscape,...
                 x1, y1, x2, y2, boundary, feed_amount, feed_time);
-        if leave == 1
-            for remaining_steps = t+1 : steps+1
-                trajectories(remaining_steps, animal_x : animal_y) = ...
-                    trajectories(t, animal_x : animal_y);
-            end
+            
+             if leave == 1
+            trajectories( (t+1 : steps+1), (animal_x : animal_y) ) = NaN;
+            time_until_leaving(animal) = t;
             break
             %ends "t" loop. returns to "animal" loop.
-        end
+       end
+            
         trajectories(t+1, animal_x : animal_y) = curr_location; %update location
     end
 
@@ -107,6 +116,7 @@ for animal = 1:num_animals
     plot3(trajectories(:,xx,1), trajectories(:,yy,1),zz)
 end 
 hold off
+
 %nutrition
 surf(landscape(:,:,2));
 hold on
@@ -118,4 +128,4 @@ for animal = 1:num_animals
 end 
 hold off
 %dung 
-%surf(landscape(:, :, 3));
+surf(landscape(:, :, 3));
