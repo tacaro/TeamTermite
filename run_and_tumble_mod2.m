d264272ea83d60b915629f1f499d2e19217e6262
Ellen Waddle <elwa8931@colorado.edu>
2020-04-09 14:15:16 -0600

    fullness and proximity to termite mounds added

--- run_and_tumble_mod2.m
+++ run_and_tumble_mod2.m
@@ -1,61 +1,57 @@
 
 %Set variable model parameters
-%initialize variables for move_and_feed_1
+%%%%grazing parameters
 feed_time = 1; %relative to total movement time (of 1)
 boundary = 8;
 feed_amount = 5;
-init_fullness = 10;
+init_fullness = 5;
 
-vary_angle_or_dist = 0; %set tumble strategy: 0: dist, 1: dist
-max_turn_angle = 2*pi;
+
+%%%movement angle/dist parameters
+vary_angle_or_dist = 0; %set tumble strategy: 0: dist, 1: dist **LIAM: both dist?
+max_turn_angle = pi;
 min_tumb = 0.5;
 max_tumb = 2;
-run_tumb_ratio = 4;
+run_tumb_ratio = max_tumb / min_tumb;
 min_run = min_tumb * run_tumb_ratio;
 max_run = max_tumb * run_tumb_ratio;
 
 %parameters for decision-making
 stay_grass = 30;
-stay_nutrition = 4;
-run_nutrition = 6; %didn't we say that nutrition varied from 0-100? these might be far too low
+stay_nutrition = 5;
+run_nutrition = 3; 
 
-xdim = 50;
-xdim = ydim;  
-n_mounds = 10;
+%%%landscape parameters (dimension, # animals, mound placement) 
+xdim = 500;
+ydim = 500;  
+n_mounds = 10; % number of termite mounds
 
-feed_amount = 5; 
-steps = 2000; %set max time steps
-num_animals = 10 ;%set number of animals to walk the Earth
+steps = 500; %set max time steps
+num_animals = 50 ;%set number of animals to walk the Earth
+fertilizer_pattern = 0;  %can be 0: random or 1: uniform. 
 
 
-[X,Y] = meshgrid(linspace(1,xdim,n_mounds/2),linspace(1,ydim,n_mounds/2));
-uniform_fert = round([X(:), Y(:)]);
+%set up fertilizer mound locations
+if fertilizer_pattern == 1 
+    [X,Y] = meshgrid(linspace(1,xdim,n_mounds/2),linspace(1,ydim,n_mounds/2));
+    fertilizer_xy = round([X(:), Y(:)]);
+elseif fertilizer_pattern == 0
+        random_fert = [randi([1 xdim],1,n_mounds) ; randi([1 ydim],1,n_mounds)];
+        fertilizer_xy = transpose(random_fert);
+end 
 
-random_fert = [randi([1 xdim],1,n_mounds) ; randi([1 ydim],1,n_mounds)];
-random_fert = transpose(random_fert);
 
-fertilizer_xy = random_fert; %options: uniform_fert or random_fert 
-
+%ready to initialize landscape
 landscape = initialize_landscape_1(xdim, ydim, fertilizer_xy);
-
 
 
 %STEP2: agents move through landscape.
 
-
-=======
-[X,Y] = meshgrid(linspace(1,45,5),linspace(1,45,5));
-fertilizer_xy = round([X(:), Y(:)]);
-scatter(X(:),Y(:))
-landscape = initialize_landscape_1(xdim, ydim, fertilizer_xy);
-
-
-
 %Record trajectory of all animals. First two columns are first animal,
 %third and fourth columns are second animal, etc.
-trajectories = zeros(steps+1, 2*num_animals);
-time_until_leaving = zeros(num_animals);
-
+trajectories = zeros(steps, 2*num_animals);
+time_until_leaving = zeros(num_animals,1); %record time animal leaves
+dist_to_closest_mound = zeros(steps, num_animals);
 
 
 for animal = 1:num_animals
@@ -65,14 +61,14 @@ for animal = 1:num_animals
     
     %random starting position on perimeter of landscape: 
     %pick x or y to start on, other var is 1 or max. 
-    A = [1, xdim];
+    A = [1+boundary, xdim-boundary];
     astart = A(randi(length(A), 1));
     bstart = (randi(xdim, 1));
     starting_pos = [astart,bstart ; bstart,astart ]; 
     start_pos = starting_pos(:,randi(2,1));
     
     trajectories(1, animal_x : animal_y) = [start_pos]; 
-    curr_location = zeros(1,2);
+    %curr_location = [start_pos(1), start_pos(2)];
     %initialize. Goes to 1 when animal leaves boundary on landscape.
     leave = 0;
     
@@ -82,55 +78,95 @@ for animal = 1:num_animals
         x1 = curr_location(1);
         y1 = curr_location(2);
         [grass_quantity, nutrition] = current_location(landscape,x1, y1);
+        fullness = init_fullness;
 
-
-        if  grass_quantity > stay_grass && nutrition > stay_nutrition
-
-            %disp('stay')
+        if  grass_quantity >= stay_grass || fullness < 2 %&& nutrition >= stay_nutrition
+            
             x2 = curr_location(1);
             y2 = curr_location(2);
-             
+            fullness = fullness+2; 
+            
         else
-           % disp('move')
-            turning_angle =  unifrnd(-max_turn_angle, max_turn_angle); %uniform dist within bounds
+            if fullness < 5 %if hungry, turns are wider. 
+                   turning_angle =  unifrnd(-max_turn_angle, max_turn_angle); 
+            else 
+                turning_angle = unifrnd(-max_turn_angle/3, max_turn_angle/3);
+            end 
             
             if nutrition < run_nutrition %move farther if nutrition is low
                 d = unifrnd(min_run, max_run); %uniform dist of step size
 
             else  %stay closer if nutrition is high
                 d = unifrnd(min_tumb, max_tumb);
+                
             end 
             
-            curr_location(1) = curr_location(1)+d*sin(turning_angle);
-            curr_location(2) = curr_location(2)+d*cos(turning_angle); %agent moves 
-            x2 = (curr_location(1));
-            y2 = (curr_location(2));
+            fullness = max(0, fullness-1);
+            x2 = curr_location(1)+d*sin(turning_angle);
+            y2 = curr_location(2)+d*cos(turning_angle); %agent moves 
+        
+            %x2 = (curr_location(1));
+            %y2 = (curr_location(2));
+            curr_location(1) = x2;
+            curr_location(2) = y2;
             
         end 
+        
         %Update landscape and trajectories array
 
         [landscape, grass_consumed, nutrition, leave] = move_and_feed_1(landscape,...
                 x1, y1, x2, y2, boundary, feed_amount, feed_time);
-         
+        
+            trajectories(t+1, animal_x : animal_y) = curr_location; 
+            
+        %calculate distance to nearest mound        
+        mound_dists = zeros(n_mounds,1);
+        for m = 1:n_mounds 
+            mound = fertilizer_xy(m,:);
+            pts = [mound; curr_location];
+            mound_dists(m) = pdist(pts, 'euclidean');
+        end 
+        dist_to_closest_mound(t, animal) = min(mound_dists);
+        
+        %calculate dist to nearest boundary
+        boundary_line =
+        
         if leave == 1
             for remaining_steps = t+1 : steps+1
                 trajectories(remaining_steps, animal_x : animal_y) = NaN;
+                time_until_leaving(animal) = t+1
             end
-            break
+        break
             %ends "t" loop. returns to "animal" loop.
+       
+            
+            
+            
        end
             
-        trajectories(t+1, animal_x : animal_y) = curr_location; %update location
     end
 
 end
 
- %visualization of path
-        
-plot(trajectories(:,1), trajectories(:,2));
+
+%%% visualization 
+
+%time spent on landscape
+hist(time_until_leaving,num_animals/5)
+title('time steps spent in simluation')
 
 
-%quantity
+%plot distance to mound center through time for each animal
+hold on 
+for animal = 1:num_animals
+    plot(1:steps, dist_to_closest_mound(:, animal))
+    
+end 
+title('distance to closest mound thru time')
+hold off 
+
+%quantity. this plot is hard to look at at first, need to ajust it w your
+%mouse
 surf(landscape(:,:,1));
 hold on 
 zz =transpose(linspace(100,100,length(trajectories(:,2))));
@@ -139,9 +175,11 @@ for animal = 1:num_animals
     yy = animal + 1;
     plot3(trajectories(:,xx,1), trajectories(:,yy,1),zz)
 end 
+title('ending landscape grass quantity values');
 hold off
 
-%nutrition
+%nutrition this plot is hard to look at at first, need to ajust it w your
+%mouse 
 surf(landscape(:,:,2));
 hold on
 zz =transpose(linspace(100,100,length(trajectories(:,2))));
@@ -150,6 +188,19 @@ for animal = 1:num_animals
     yy = animal + 1;
     plot3(trajectories(:,xx,1), trajectories(:,yy,1),zz)
 end 
+title('ending landscape nutrition values');
 hold off
-%dung 
-surf(landscape(:, :, 3));
+
+
+%dung  this plot is hard to look at at first, need to ajust it w your
+%mouse
+surf(landscape(:, :, 3));zz =transpose(linspace(100,100,length(trajectories(:,2))));
+hold on
+for animal = 1:num_animals
+    xx = animal;
+    yy = animal + 1;
+    plot3(trajectories(:,xx,1), trajectories(:,yy,1),zz)
+end 
+title('dung location pileups');
+hold off 
+
