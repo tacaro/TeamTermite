797ea2e243d40a96aa22d973c56e39c5e97025ca
tacaro <tristan.caro@colorado.edu>
2020-04-19 12:54:58 -0600

    additional plots, expanded R script

--- R_analysis/residence_time.R
+++ R_analysis/residence_time.R
@@ -5,20 +5,18 @@ require(tidyverse)
 
 # Import data as .csv
 # EDIT this as necessary to read in the correct files
-randomdf <- as_tibble(read.csv('/Users/Tristan/TeamTermite/dfs/random_200_100_residency.csv'))
-uniformdf <- as_tibble(read.csv('/Users/Tristan/TeamTermite/dfs/uniform_200_100_residency.csv'))
+randomdf <- as_tibble(read.csv('/Users/Tristan/TeamTermite/dfs/random_200_300_residency.csv'))
+uniformdf <- as_tibble(read.csv('/Users/Tristan/TeamTermite/dfs/uniform_200_300_residency.csv'))
 
 randomdf <- randomdf %>%
-  select(time_in_high_nutrient = X0) %>% 
-  #select(-X0) %>%
+  select(time_in_high_nutrient = colnames(randomdf[2])) %>% # rename the time column
   mutate(plot_type = "random")
 
 uniformdf <- uniformdf %>%
-  select(time_in_high_nutrient = X14) %>% 
-  #select(-X1) %>%
+  select(time_in_high_nutrient = colnames(uniformdf)[2]) %>% # rename the time column
   mutate(plot_type = "uniform")
 
-combined <- full_join(randomdf, uniformdf)
+combined <- full_join(randomdf, uniformdf) # join the two dataframes
 
 boxplot <- ggplot(combined, aes(x = plot_type, y = time_in_high_nutrient, color = plot_type)) +
   geom_boxplot() +
@@ -27,4 +25,19 @@ boxplot <- ggplot(combined, aes(x = plot_type, y = time_in_high_nutrient, color 
   xlab('Landscape type') +
   theme_classic()
 ggsave('residency_boxplot.png')
+
+
+# What if we remove all the zero values? This means we do not consider animals that spent no time in the nutrient
+# i.e. for animals that did interact with nutrient, which pattern held them the longest?
+
+combined_no_zero <- combined %>% 
+  filter(time_in_high_nutrient != 0) # filter out zero values
+
+boxplot_no_zero <- ggplot(combined_no_zero, aes(x = plot_type, y = time_in_high_nutrient, color = plot_type)) +
+  geom_boxplot() +
+  geom_jitter(shape = 16, position = position_jitter(0.2)) +
+  ylab('Time spent in high nutrient (ticks)') +
+  xlab('Landscape type') +
+  theme_classic()
+ggsave('residency_boxplot_no_zero.png')
   
